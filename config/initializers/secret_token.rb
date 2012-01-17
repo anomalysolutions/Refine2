9a6956e66b26cf172ffce6da52f1fda5838251a4
Amol Atigre <amol@anomaly.co.in>
2012-01-17 11:45:23 +0430

    Second COmmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refine2::Application.config.secret_token = '9dc06ae634deb49873722aef05fa501e674ac6126a4a2c422995cadf861a00107fc6ad27c474e8df9059b70b7b56532d2e6975f9e9ac4c559dac132a000e9558'
